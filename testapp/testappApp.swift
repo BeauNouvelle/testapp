785f18f78a9482daa9cde7b8b8addd3807ddce44
Beau Nouvelle <beau@getswifty.dev>
2021-02-17 19:12:54 +1100

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Beau Nouvelle on 17/2/21.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
